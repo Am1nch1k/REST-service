151f5d62d2bbe5245b376e82d37938d13055cfe3
denissed <amin.sadigoff@yandex.ru>
2025-07-29 14:36:17 +0700

    PostMapping

--- src/main/java/com/example/REST/controller/MainController.java
+++ src/main/java/com/example/REST/controller/MainController.java
@@ -1,11 +1,12 @@
 package com.example.REST.controller;
-
 import com.example.REST.entity.animals;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
